a54dd805f2711f3c4d83ab47d58c91a1b7a794a8
Bridgett Wheeler <bridgettwheeler@gmail.com>
2022-03-01 14:18:47 -0500

    commit tree 1e905710646f38b8e36ba1d88588f1bba0b46738

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,29 @@
 class Movie < ActiveRecord::Base
+    def Movie.create_with_title
+        Movie.create(title: "Raya")
+    end
 
+    def Movie.first_movie
+        Movie.first
+    end
+
+    def Movie.last_movie
+        Movie.last
+    end
+
+    def Movie.movie_count
+        Movie.count
+    end
+
+    def Movie.find_movie_with_id(id)
+        Movie.find(id)
+    end
+
+    def Movie.find_movie_with_attributes(att)
+        Movie.find_by(att)
+    end
+
+    def Movie.find_movies_after_2002
+            Movie.where(year: > 2002)
+          end
 end
